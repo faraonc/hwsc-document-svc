4f7ce9dbd9582d0c4c8b73ca91b43775b944aef4
CJ Faraon <faraonc@outlook.com>
2018-10-31 21:30:25 -0700

    WIP integration testing

--- service/service.go
+++ service/service.go
@@ -39,8 +39,8 @@ const (
 		"==@hwscmongodb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
 	mongoServerDBReader = "mongodb://hwscmongodb:mV2GqGnzoOXPF82QZbEzEi0QcFSLK4fyh2EAzU3KrZfw1wSePaQbKINUrWKfblBS3diQfJCd7ugAOYHMZK2eLA" +
 		"==@hwscmongodb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
-	mongoDB           = "METADATA-FILE"
-	mongoDBCollection = "metadata-file"
+	mongoDB           = "hwsc-document"
+	mongoDBCollection = "hwsc-document"
 
 	// available - Service is ready and available
 	available state = 0
@@ -130,7 +130,8 @@ func (s Service) GetStatus(ctx context.Context, req *pb.DocumentRequest) (*pb.Do
 
 }
 
-// CreateDocument creates a Document in MongoDB.
+// CreateDocument creates a document in MongoDB.
+// Returns the Document.
 func (s Service) CreateDocument(ctx context.Context, req *pb.DocumentRequest) (*pb.DocumentResponse, error) {
 	log.Println("[INFO] Requesting CreateDocument service")
 
@@ -144,13 +145,13 @@ func (s Service) CreateDocument(ctx context.Context, req *pb.DocumentRequest) (*
 		return nil, status.Error(codes.InvalidArgument, "Nil request data")
 	}
 
+	doc.Duid = duidGenerator.NewDUID()
+
 	// Validate document
 	if err := ValidateDocument(doc); err != nil {
 		log.Printf("[ERROR] %s\n", err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
-
-	doc.Duid = duidGenerator.NewDUID()
 
 	// Get the specific lock if it already exists, else make the lock
 	lock, _ := serviceClientLocker.LoadOrStore(doc.GetDuid(), &sync.RWMutex{})
@@ -164,7 +165,7 @@ func (s Service) CreateDocument(ctx context.Context, req *pb.DocumentRequest) (*
 	log.Printf("[INFO] Document contains:\n %s\n\n", doc)
 
 	// Connect to MongoDB
-	log.Printf("[INFO] Connecting to mongodb://hwscmongodb as %s with %s\n", doc.GetUuid(), doc.GetDuid())
+	log.Printf("[INFO] Connecting to mongodb://hwscmongodb duid: %s uuid: %s\n", doc.GetDuid(), doc.GetUuid())
 	client, err := mongo.NewClient(mongoServerDBWriter)
 	if err != nil {
 		log.Printf("[ERROR] mongo.NewClient(mongoServerDBWriter): %s\n", err.Error())
@@ -186,10 +187,11 @@ func (s Service) CreateDocument(ctx context.Context, req *pb.DocumentRequest) (*
 	}
 
 	if err := client.Disconnect(context.TODO()); err != nil {
-		log.Printf("[ERROR] Inserted documment _id: %v, with disconnect error\n", res.InsertedID)
+		log.Printf("[ERROR] Inserted documment _id: %v, with disconnection error\n", res.InsertedID)
 		return &pb.DocumentResponse{
 			Status:  &pb.DocumentResponse_Code{Code: uint32(codes.Internal)},
 			Message: fmt.Sprintf("Inserted with error: %s\n", codes.Internal.String()),
+			Data:    doc,
 		}, nil
 	}
 
@@ -198,31 +200,247 @@ func (s Service) CreateDocument(ctx context.Context, req *pb.DocumentRequest) (*
 	return &pb.DocumentResponse{
 		Status:  &pb.DocumentResponse_Code{Code: uint32(codes.OK)},
 		Message: codes.OK.String(),
+		Data:    doc,
 	}, nil
 
 }
 
-// ListUserDocumentCollection gets all the MongoDB documents for a specific user with the given UUID.
-// Returns a collection of MongoDB documents.
+// ListUserDocumentCollection retrieves all the MongoDB documents for a specific user with the given UUID.
+// Returns a collection of Documents.
 func (s Service) ListUserDocumentCollection(ctx context.Context, req *pb.DocumentRequest) (*pb.DocumentResponse, error) {
+	log.Println("[INFO] Requesting ListUserDocumentCollection service")
 
-	return nil, nil
+	if ok := isStateAvailable(); !ok {
+		return nil, status.Error(codes.Unavailable, "Service unavailable")
+	}
+
+	doc := req.GetData()
+	if doc == nil {
+		log.Println("[ERROR] Nil request data")
+		return nil, status.Error(codes.InvalidArgument, "Nil request data")
+	}
+
+	// Validate UUID field
+	if err := ValidateUUID(doc.GetUuid()); err != nil {
+		log.Printf("[ERROR] %s\n", err.Error())
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	// Connect to MongoDB
+	log.Printf("[INFO] Connecting to mongodb://hwscmongodb uuid: %s\n", doc.GetUuid())
+	client, err := mongo.NewClient(mongoServerDBReader)
+	if err != nil {
+		log.Printf("[ERROR] mongo.NewClient(mongoServerDBReader): %s\n", err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if err := client.Connect(context.TODO()); err != nil {
+		log.Printf("[ERROR] client.Connect(context.TODO()): %s\n", err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	collection := client.Database(mongoDB).Collection(mongoDBCollection)
+
+	// Find all MongoDB documents for the specific uuid
+	cur, err := collection.Find(context.Background(), mongoQuery{
+		"uuid": doc.GetUuid(),
+	})
+	if err != nil {
+		log.Printf("[ERROR] collection.Find(context.Background(), mongoQuery{\"uuid\": doc.GetUuid(),}): %s\n", err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	// Close the MongoDB cursor before the function exits
+	defer cur.Close(context.Background())
+
+	// Extract the documents
+	documentCollection := make([]*pb.Document, 0)
+	for cur.Next(context.Background()) {
+		if err := cur.Err(); err != nil {
+			log.Printf("[ERROR] cur.Err(): %s\n", err.Error())
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+
+		// Mutate and retrieve Document
+		document := &pb.Document{}
+		if err := cur.Decode(document); err != nil {
+			log.Printf("[ERROR] %s\n", err.Error())
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+
+		// Validate the retrieved Document from MongoDB
+		if err := ValidateDocument(document); err != nil {
+			log.Printf("[ERROR] Failed document validation, duid: %v - uuid: %v - %s\n",
+				doc.GetDuid(), doc.GetUuid(), err.Error())
+
+			return nil, status.Errorf(codes.Internal, "Failed document validations, duid: %v - uuid: %v - %s",
+				doc.GetDuid(), doc.GetUuid(), err.Error())
+		}
+		documentCollection = append(documentCollection, document)
+		log.Printf("[DEBUG] document: \n%s\n\n", document)
+
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Printf("[ERROR] cur.Err(): %s\n", err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if len(documentCollection) == 0 {
+		log.Printf("[ERROR] No documments for uuid: %v\n\n", doc.GetUuid())
+		return nil, status.Errorf(codes.Internal, "No documments for uuid: %v", doc.GetUuid())
+	}
+
+	if err := client.Disconnect(context.TODO()); err != nil {
+		log.Printf("[ERROR] Success listing documments, uuid: %v with disconnection error\n", doc.GetUuid())
+		return &pb.DocumentResponse{
+			Status:             &pb.DocumentResponse_Code{Code: uint32(codes.Internal)},
+			Message:            fmt.Sprintf("Listed with disconnection error: %s\n", codes.Internal.String()),
+			DocumentCollection: documentCollection,
+		}, nil
+	}
+
+	// Log ID of the uuid used for query
+	log.Printf("[INFO] Success listing documents, uuid: %v\n\n", doc.GetUuid())
+
+	return &pb.DocumentResponse{
+		Status:             &pb.DocumentResponse_Code{Code: uint32(codes.OK)},
+		Message:            codes.OK.String(),
+		DocumentCollection: documentCollection,
+	}, nil
 
 }
 
-// UpdateDocument completely updates a MongoDB document with a given DUID.
-// Returns the updated MongoDB document.
-//TODO implementation
-//TODO unit test
+// UpdateDocument (completely) updates a MongoDB document with a given DUID.
+// Returns the updated Document.
 func (s Service) UpdateDocument(ctx context.Context, req *pb.DocumentRequest) (*pb.DocumentResponse, error) {
+	log.Println("[INFO] Requesting UpdateDocument service")
 
-	return nil, nil
+	if ok := isStateAvailable(); !ok {
+		return nil, status.Error(codes.Unavailable, "Service unavailable")
+	}
+
+	doc := req.GetData()
+	if doc == nil {
+		log.Println("[ERROR] Nil request data")
+		return nil, status.Error(codes.InvalidArgument, "Nil request data")
+	}
+
+	if doc.GetDuid() == "" {
+		log.Printf("[ERROR] Missing DUID")
+		return nil, status.Error(codes.InvalidArgument, "Missing DUID")
+	}
+
+	// Generate UpdateTimestamp
+	doc.UpdateTimestamp = time.Now().UTC().Unix()
+
+	// Validate document
+	if err := ValidateDocument(doc); err != nil {
+		log.Printf("[ERROR] %s\n", err.Error())
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	log.Printf("[INFO] Document contains:\n %s\n\n", doc)
+
+	// Get the specific lock if it already exists, else make the lock
+	lock, _ := serviceClientLocker.LoadOrStore(doc.GetDuid(), &sync.RWMutex{})
+
+	// Lock
+	lock.(*sync.RWMutex).Lock()
+	// Unlock before the function exits
+	defer lock.(*sync.RWMutex).Unlock()
+
+	// Connect to MongoDB
+	log.Printf("[INFO] Connecting to mongodb://hwscmongodb duid: %s uuid: %s\n",
+		doc.GetDuid(), doc.GetUuid())
+	client, err := mongo.NewClient(mongoServerDBWriter)
+	if err != nil {
+		log.Printf("[ERROR] mongo.NewClient(mongoServerDBWriter): %s\n", err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if err := client.Connect(context.TODO()); err != nil {
+		log.Printf("[ERROR] client.Connect(context.TODO()): %s\n", err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	collection := client.Database(mongoDB).Collection(mongoDBCollection)
+
+	result := collection.FindOneAndReplace(context.Background(), mongoQuery{
+		"duid": doc.GetDuid(),
+		"uuid": doc.GetUuid(),
+	}, doc)
+
+	// Extract the updated MongoDB document
+	if result == nil {
+		log.Printf("[INFO] Document not found, duid: %v - uuid: %v\n\n",
+			doc.GetDuid(), doc.GetUuid())
+
+		return nil, status.Errorf(codes.InvalidArgument,
+			"Document not found, duid: %v - uuid: %v",
+			doc.GetDuid(), doc.GetUuid())
+	}
+
+	log.Println("[INFO] Extracting updated MongoDB document")
+
+	document := &pb.Document{}
+	if err := result.Decode(document); err != nil {
+		log.Printf("[ERROR] Document not found, duid: %v - uuid: %v\n\n",
+			doc.GetDuid(), doc.GetUuid())
+
+		return nil, status.Errorf(codes.InvalidArgument,
+			"Document not found, duid: %v - uuid: %v",
+			doc.GetDuid(), doc.GetUuid())
+	}
+
+	// Validate the retrieved Document from MongoDB
+	if err := ValidateDocument(document); err != nil {
+		log.Printf("[ERROR] Failed document validation, duid: %v - uuid: %v - %s\n",
+			doc.GetDuid(), doc.GetUuid(), err.Error())
+
+		return nil, status.Errorf(codes.Internal, "Failed document validations, duid: %v - uuid: %v - %s",
+			doc.GetDuid(), doc.GetUuid(), err.Error())
+	}
+
+	if err := client.Disconnect(context.TODO()); err != nil {
+		log.Printf("[ERROR] Success updating document, duid: %v - uuid: %v with disconnection error\n\n", doc.GetDuid(), doc.GetUuid())
+		return &pb.DocumentResponse{
+			Status:  &pb.DocumentResponse_Code{Code: uint32(codes.Internal)},
+			Message: fmt.Sprintf("Updated with disconnection error: %s\n", codes.Internal.String()),
+			Data:    document,
+		}, nil
+	}
+
+	// Log duid and uuid used for query
+	log.Printf("[INFO] Success updating document, duid: %v - uuid: %v\n\n", doc.GetDuid(), doc.GetUuid())
+
+	return &pb.DocumentResponse{
+		Status:  &pb.DocumentResponse_Code{Code: uint32(codes.OK)},
+		Message: codes.OK.String(),
+		Data:    document,
+	}, nil
 
 }
 
 // DeleteDocument deletes a MongoDB document using UUID and DUID.
-// Returns the deleted MongoDB document.
+// Returns the deleted Document.
 func (s Service) DeleteDocument(ctx context.Context, req *pb.DocumentRequest) (*pb.DocumentResponse, error) {
+
+	return nil, nil
+
+}
+
+// AddFileMetadata adds a new FileMetadata in a MongoDB document using a given url, UUID and DUID.
+// Returns the updated Document.
+func (s Service) AddFileMetadata(ctx context.Context, req *pb.DocumentRequest) (*pb.DocumentResponse, error) {
+
+	return nil, nil
+
+}
+
+// DeleteFileMetadata deletes a FileMetadata in a MongoDB document using a given FUID, UUID and DUID.
+// Returns the updated Document.
+func (s Service) DeleteFileMetadata(ctx context.Context, req *pb.DocumentRequest) (*pb.DocumentResponse, error) {
 
 	return nil, nil
 
--- service/service_test.go
+++ service/service_test.go
@@ -24,3 +24,171 @@ func TestGetStatus(t *testing.T) {
 		assert.Equal(t, c.expMsg, res.GetMessage())
 	}
 }
+
+func TestCreateDocument(t *testing.T) {
+	cases := []struct {
+		req         *pb.DocumentRequest
+		serverState state
+		expMsg      string
+		isExpErr    bool
+	}{
+		{&pb.DocumentRequest{}, unavailable,
+			"rpc error: code = Unavailable desc = Service unavailable", true},
+		{nil, available,
+			"rpc error: code = InvalidArgument desc = Nil request data", true},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Uuid: "garbage",
+		}}, available,
+			"rpc error: code = InvalidArgument desc = invalid Document uuid", true},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Duid:         "",
+			Uuid:         "0000XSNJG0MQJHBF4QX1EFD6Y3",
+			LastName:     "Kim",
+			FirstName:    "Lisa",
+			CallTypeName: "some call type name",
+			GroundType:   "some ground type",
+			Region:       "some region",
+			Ocean:        "Pacific Ocean",
+			SensorType:   "some sensor type",
+			SensorName:   "some sensor name",
+			SampleRate:   100,
+			Latitude:     89.123,
+			Longitude:    -100.123,
+			ImageUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/image/hulkgif.png",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/image/Rotating_earth_(large).gif"},
+			AudioUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/audio/Seger_Conga_CaboMexico_Tag_Acousonde_20140313_112313_8000_3_BreedingMigrating.wav",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/audio/Milad Hosseini - Deli Asheghetam [128].mp3"},
+			VideoUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.wmv",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.mp4"},
+			FileUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.wmv",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.mp4"},
+			RecordTimestamp: 1514764800,
+			CreateTimestamp: 1539831496,
+			UpdateTimestamp: 0,
+		}}, available,
+			"OK", false},
+	}
+
+	for _, c := range cases {
+		serviceStateLocker.currentServiceState = c.serverState
+		s := Service{}
+		res, err := s.CreateDocument(context.TODO(), c.req)
+		if !c.isExpErr {
+			assert.Equal(t, c.expMsg, res.GetMessage())
+		} else {
+			assert.Equal(t, c.expMsg, err.Error())
+			assert.EqualError(t, err, c.expMsg)
+		}
+
+	}
+}
+
+func TestListUserDocumentCollection(t *testing.T) {
+	cases := []struct {
+		req         *pb.DocumentRequest
+		serverState state
+		expMsg      string
+		isExpErr    bool
+	}{
+		{&pb.DocumentRequest{}, unavailable,
+			"rpc error: code = Unavailable desc = Service unavailable", true},
+		{nil, available,
+			"rpc error: code = InvalidArgument desc = Nil request data", true},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Uuid: "garbage",
+		}}, available,
+			"rpc error: code = InvalidArgument desc = invalid Document uuid", true},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Uuid: "0000XSNJG0MQJHBF4QX1EFD6Y3",
+		}}, available,
+			"OK", false},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Uuid: "4ee30333-8ec8-45a4-ba94-5e22c4a686de",
+		}}, available,
+			"rpc error: code = InvalidArgument desc = invalid Document uuid", true},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Uuid: "xxx0XSNJG0MQJHBF4QX1EFD6Y3",
+		}}, available,
+			"rpc error: code = Internal desc = No documments for uuid: xxx0XSNJG0MQJHBF4QX1EFD6Y3", true},
+	}
+
+	for _, c := range cases {
+		serviceStateLocker.currentServiceState = c.serverState
+		s := Service{}
+		res, err := s.ListUserDocumentCollection(context.TODO(), c.req)
+		if !c.isExpErr {
+			assert.Equal(t, c.expMsg, res.GetMessage())
+			assert.NotEmpty(t, res.DocumentCollection)
+		} else {
+			assert.Equal(t, c.expMsg, err.Error())
+			assert.EqualError(t, err, c.expMsg)
+		}
+
+	}
+}
+
+func TestUpdateDocument(t *testing.T) {
+	cases := []struct {
+		req         *pb.DocumentRequest
+		serverState state
+		expMsg      string
+		isExpErr    bool
+	}{
+		{&pb.DocumentRequest{}, unavailable,
+			"rpc error: code = Unavailable desc = Service unavailable", true},
+		{nil, available,
+			"rpc error: code = InvalidArgument desc = Nil request data", true},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Duid: "",
+		}}, available,
+			"rpc error: code = InvalidArgument desc = Missing DUID", true},
+		{&pb.DocumentRequest{Data: &pb.Document{
+			Duid:         "1CMjsoGz1cNOkIYaarbcSzmNg1n",
+			Uuid:         "0000XSNJG0MQJHBF4QX1EFD6Y3",
+			LastName:     "Kim",
+			FirstName:    "Lisa",
+			CallTypeName: "some call type name",
+			GroundType:   "some ground type",
+			Region:       "San Diego, CA",
+			Ocean:        "Pacific Ocean",
+			SensorType:   "some sensor type",
+			SensorName:   "some sensor name",
+			SampleRate:   100,
+			Latitude:     89.123,
+			Longitude:    -100.123,
+			ImageUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/image/hulkgif.png",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/image/Rotating_earth_(large).gif"},
+			AudioUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/audio/Seger_Conga_CaboMexico_Tag_Acousonde_20140313_112313_8000_3_BreedingMigrating.wav",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/audio/Milad Hosseini - Deli Asheghetam [128].mp3"},
+			VideoUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.wmv",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.mp4"},
+			FileUrl: map[string]string{
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686de": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.wmv",
+				"4ff30392-8ec8-45a4-ba94-5e22c4a686df": "https://hwssappstorage.blob.core.windows.net/video/videoplayback.mp4"},
+			RecordTimestamp: 1514764800,
+			CreateTimestamp: 1539831496,
+			UpdateTimestamp: 0,
+		}}, available,
+			"OK", false},
+	}
+
+	for _, c := range cases {
+		serviceStateLocker.currentServiceState = c.serverState
+		s := Service{}
+		res, err := s.UpdateDocument(context.TODO(), c.req)
+		if !c.isExpErr {
+			assert.Equal(t, c.expMsg, res.GetMessage())
+		} else {
+			assert.Equal(t, c.expMsg, err.Error())
+			assert.EqualError(t, err, c.expMsg)
+		}
+
+	}
+}
