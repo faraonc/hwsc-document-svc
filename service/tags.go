de4802f26c2d9b74625b9fd46adbf2183c073b89
CJ Faraon <faraonc@outlook.com>
2019-01-26 21:27:16 -0800

    Change tags to be consistent with user-svc

--- service/tags.go
+++ service/tags.go
@@ -1,15 +1,15 @@
 package service
 
 const (
-	getStatusTag                  string = "GetStatus:"
-	createDocumentTag             string = "CreateDocument:"
-	listUserDocumentCollectionTag string = "ListUserDocumentCollection:"
-	updateDocumentTag             string = "UpdateDocument:"
-	deleteDocumentTag             string = "DeleteDocument:"
-	addFileMetadataTag            string = "AddFileMetadata:"
-	deleteFileMetadataTag         string = "DeleteFileMetadata:"
-	listDistinctFieldValuesTag    string = "ListDistinctFieldValues:"
-	queryDocumentTag              string = "QueryDocument:"
-	serviceStateTag               string = "Service State:"
-	mongoDBTag                    string = "MongoDB:"
+	getStatusTag                  string = "GetStatus -"
+	createDocumentTag             string = "CreateDocument -"
+	listUserDocumentCollectionTag string = "ListUserDocumentCollection -"
+	updateDocumentTag             string = "UpdateDocument -"
+	deleteDocumentTag             string = "DeleteDocument -"
+	addFileMetadataTag            string = "AddFileMetadata -"
+	deleteFileMetadataTag         string = "DeleteFileMetadata -"
+	listDistinctFieldValuesTag    string = "ListDistinctFieldValues -"
+	queryDocumentTag              string = "QueryDocument -"
+	serviceStateTag               string = "Service State -"
+	mongoDBTag                    string = "MongoDB -"
 )
